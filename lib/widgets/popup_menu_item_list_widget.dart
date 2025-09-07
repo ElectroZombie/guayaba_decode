520631d25e33aee0fb9477a982f94ae5f4cb279f
ElectroZombie <daphniphyllum901@gmail.com>
2025-09-07 11:47:07 -0400

    generado un coming soon

--- lib/widgets/popup_menu_item_list_widget.dart
+++ lib/widgets/popup_menu_item_list_widget.dart
@@ -6,21 +6,32 @@ List<PopupMenuItem> popupMenuItemList(
     Map<int, Tuple<Tuple<Function, Function>, IconModel>> M,
     callBackTypeEncrypt) {
   return List.generate(
-    M.length * 2 - 1,
+    M.length * 2 + 1,
     (i) => selectorMenuItem(i, M, callBackTypeEncrypt),
   );
 }
 
 selectorMenuItem(int i, M, callBackTypeEncrypt) {
   if (i % 2 == 0) {
-    return PopupMenuItem(
-        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text("METHOD ${i ~/ 2 + 1}:",
-              style: const TextStyle(
-                  fontSize: 15, color: Color.fromARGB(232, 186, 180, 180))),
-          Icon(M[i / 2 + 1]!.K!.getIcon(), color: M[i / 2 + 1]!.K!.getColor())
-        ]),
-        onTap: () => {callBackTypeEncrypt(i ~/ 2 + 1)});
+    if (i == M.length * 2) {
+      return const PopupMenuItem(
+          enabled: false,
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              "COMING SOON...",
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.end,
+            ),
+            Icon(Icons.watch_later_outlined)
+          ]));
+    } else {
+      return PopupMenuItem(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text("METHOD ${i ~/ 2 + 1}:", style: const TextStyle(fontSize: 15)),
+            Icon(M[i / 2 + 1]!.K!.getIcon(), color: M[i / 2 + 1]!.K!.getColor())
+          ]),
+          onTap: () => {callBackTypeEncrypt(i ~/ 2 + 1)});
+    }
   } else {
     return const PopupMenuItem(
       height: 10,
